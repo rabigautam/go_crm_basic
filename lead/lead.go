775a567abdc1bc5ed38fbadb96877d4b144081a0
Rabi Gautam <rabi@sharelookapp.com>
2022-05-02 20:27:18 +0545

    add:lead model

---
+++ lead/lead.go
@@ -0,0 +1,28 @@
+package lead
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
+	"github.com/rabigautam/go_crm_basic/database"
+)
+
+type Lead struct {
+	gorm.Model
+	Name    string `json:"name"`
+	Company string `json:"company"`
+	Email   string `json:"email"`
+	Phone   int    `json:"phone"`
+}
+
+func NewLead(c *fiber.Ctx) {
+	db := database.DBconn
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	db.Create(&lead)
+	c.JSON(lead)
+}
